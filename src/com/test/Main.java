0d6bf1d745f2129088c957d15002a79f7186a722
Maksat <dzhekishev@gmail.com>
2019-11-02 23:12:53 +0600

    исправлено

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -9,18 +9,21 @@ public class Main {
         names[1] = "Tumar";
         names[2] = "Shirin";
 
-        switch (names[0]) {
-            case "Alina":
-                System.out.println("Good morning " + " " + names[0]);
-            case "Tumar":
-                System.out.println("Good afternoon " + " " + names[1]);
-            case "Shirin":
-                System.out.println("Good evening " + " " + names[2]);
-                break;
-        }
-
         for (int i = 0; i < names.length; i++) {
             System.out.println("Hi : " + names[i]);
+
+
+            switch (names[i]) {
+                case "Alina":
+                    System.out.println("Good morning " + " " + names[0]);
+                    break;
+                case "Tumar":
+                    System.out.println("Good afternoon " + " " + names[1]);
+                    break;
+                case "Shirin":
+                    System.out.println("Good evening " + " " + names[2]);
+                    break;
+            }
 
         }
 
